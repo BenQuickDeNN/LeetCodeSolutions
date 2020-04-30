8fca478a71ba6ffc775b9084cef4adb73a73ae72
BenQuickDeNN <benquickdenn@foxmail.com>
2020-04-30 15:32:47 +0800

    添加 快乐数

---
+++ 快乐数.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    int quwei(const int& n)
+    {
+        int m = n;
+        int sum = 0;
+        while (m > 0)
+        {
+            sum += (m % 10) * (m % 10);
+            m /= 10;
+        }
+        return sum;
+    }
+public:
+    bool isHappy(int n) {
+        /* 使用快慢指针法 */
+        int slow = 0, fast = 0;
+        int i = 1;
+        fast = quwei(quwei(n));
+        slow = quwei(n);
+        while (true)
+        {
+            if (fast == 1)
+                return true;
+            else if (fast == slow)
+                return false;
+            fast = quwei(quwei(fast));
+            slow = quwei(slow);
+        }
+        return false;
+    }
+};
+
+int main ()
+{
+    Solution sol;
+    sol.isHappy(19);
+    return 0;
+}
