2f0f9f7585bed0db3ab36f77d01962f09e3c8113
BenQuickDeNN <2413269761@qq.com>
2020-06-02 13:31:10 +0800

    添加 求1+2+...+n

--- 求1+2+++n.cpp
+++ 求1+2+++n.cpp
@@ -3,6 +3,7 @@ public:
     int sumNums(int n) {
         /* 不能用乘除法，也不能用循环，所以只能递归 */
         n && (n += sumNums(n - 1)); // 等效于if (n && (n += sumNums(n - 1)))
+        // && 的左边为0，那么程序不会再去判断右边
         return n;
     }
 };
