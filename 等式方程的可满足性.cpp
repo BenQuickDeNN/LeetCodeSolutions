7b55a76b525c535bf5723f85f197c3eb2a295bf9
BenQuickDeNN <2413269761@qq.com>
2020-06-08 14:05:36 +0800

    添加 等式方程的可满足性

---
+++ 等式方程的可满足性.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <string>
+#include <numeric>
+using namespace std;
+class UnionFind {
+private:
+    vector<int> parent;
+
+public:
+    UnionFind() {
+        parent.resize(26);
+        iota(parent.begin(), parent.end(), 0); // 对一个范围的数进行赋值
+    }
+
+    int find(int index) {
+        if (index == parent[index]) {
+            return index;
+        }
+        parent[index] = find(parent[index]);
+        return parent[index];
+    }
+
+    void unite(int index1, int index2) {
+        parent[find(index1)] = find(index2);
+    }
+};
+
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        // 并查集
+        UnionFind uf;
+        for (const string& str: equations) {
+            if (str[1] == '=') {
+                int index1 = str[0] - 'a';
+                int index2 = str[3] - 'a';
+                uf.unite(index1, index2);
+            }
+        }
+        for (const string& str: equations) {
+            if (str[1] == '!') {
+                int index1 = str[0] - 'a';
+                int index2 = str[3] - 'a';
+                if (uf.find(index1) == uf.find(index2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
