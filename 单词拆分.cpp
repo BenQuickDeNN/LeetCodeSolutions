732a351a583de166d34056633bf2a63c1556eb5e
BenQuickDeNN <2413269761@qq.com>
2020-06-25 13:26:17 +0800

    添加 单词拆分

---
+++ 单词拆分.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // bool dp[s.size() + 1] = {false};
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for(int i=0; i<=s.size(); ++i) {
+            for(auto word: wordDict) {
+                int ws = word.size();
+                if(i - ws >= 0) {
+                    int cur = s.compare(i-ws, ws, word);
+                    if (cur==0 && dp[i-ws]==1) {
+                        dp[i] = 1;
+                    }
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
