8c8c3e83851c36dcd121dcca9e9abfe7e44976d2
qingbei <qingbei@pinduoduo.com>
2020-08-31 19:17:11 +0800

    添加 钥匙和房间

---
+++ 钥匙和房间.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    vector<int> vis;
+    int num;
+
+    void dfs(vector<vector<int>>& rooms, int x) {
+        vis[x] = true;
+        num++;
+        for (auto& it : rooms[x]) {
+            if (!vis[it]) {
+                dfs(rooms, it);
+            }
+        }
+    }
+
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        // 深度优先搜索
+        int n = rooms.size();
+        num = 0;
+        vis.resize(n);
+        dfs(rooms, 0);
+        return num == n;
+    }
+};
