01e31c1c4e8fd474ba50bfe66811e73a78b0e2a9
qingbei <qingbei@pinduoduo.com>
2020-09-01 21:55:12 +0800

    添加 预测赢家

---
+++ 预测赢家.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        // 动态规划
+        int length = nums.size();
+        auto dp = vector<int>(length);
+        for (int i = 0; i < length; i++) {
+            dp[i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[length - 1] >= 0;
+    }
+};
