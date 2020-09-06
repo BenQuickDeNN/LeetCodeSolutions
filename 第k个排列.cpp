8fbd9ce20a855e758585ff675eb90a84b0295b3e
qingbei <qingbei@pinduoduo.com>
2020-09-06 14:17:35 +0800

    第k个排列

---
+++ 第k个排列.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        // 数学 + 缩小问题规模
+        vector<int> factorial(n);
+        factorial[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        --k;
+        string ans;
+        vector<int> valid(n + 1, 1);
+        for (int i = 1; i <= n; ++i) {
+            int order = k / factorial[n - i] + 1;
+            for (int j = 1; j <= n; ++j) {
+                order -= valid[j];
+                if (!order) {
+                    ans += (j + '0');
+                    valid[j] = 0;
+                    break;
+                }
+            }
+            k %= factorial[n - i];
+        }   
+        return ans;     
+    }
+};
