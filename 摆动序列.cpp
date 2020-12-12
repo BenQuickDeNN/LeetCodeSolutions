b692732672e2117fef47438d60d03bfffcb53ba7
BenQuickDeNN <2413269761@qq.com>
2020-12-12 11:12:34 +0800

    update

--- 摆动序列.cpp
+++ 摆动序列.cpp
@@ -4,36 +4,24 @@ using namespace std;
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        const size_t N = nums.size();
-        if (N == 0)
-            return 0;
-        vector<int> wiggle(N - 1, 0);
-        for (size_t i = 1; i < N; ++i)
+        // 动态规划
+        int n = nums.size();
+        if (n < 2)
         {
-            wiggle[i - 1] = nums[i] - nums[i - 1];
+            return n;
         }
-        int ans = 1;
-        int len = 1;
-        const size_t NW = wiggle.size();
-        for (size_t i = 1; i < NW; ++i)
+        int up = 1, down = 1;
+        for (int i = 1; i < n; ++i)
         {
-            if ((wiggle[i] * wiggle[i - 1]) < 0)
+            if (nums[i] > nums[i - 1])
             {
-                ++len;
+                up = max(up, down + 1);
             }
-            else
+            else if (nums[i] < nums[i - 1])
             {
-                if (len > 1)
-                    ans = max(ans, len + 1);
-                else
-                    ans = max(ans, len);
-                len = 1;
+                down = max(up + 1, down);
             }
         }
-        if (len > 1)
-            ans = max(ans, len + 1);
-        else
-            ans = max(ans, len);
-        return ans;
+        return max(up, down);
     }
 };
